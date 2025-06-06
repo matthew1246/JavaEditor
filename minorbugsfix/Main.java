4eea7666c0400c4941690913a574483e8ed1062f
Matthew Perkovic <matthew1246@hotmail.com>
2025-06-06 10:48:37 +1000

    Fix directory for msdos class.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -746,12 +746,12 @@ public class Main {
 	
 			if(selected2 == null) JOptionPane.showMessageDialog(null,"filename combox value is null");
 			
-			dir=dir+selected2;
-				
 			if(msdos == null)
 				msdos = new MSDOS(dir);
 			else
 				msdos.setFileName(dir);
+			
+			dir=dir+selected2;
 				
 			fileName = dir; // Might need uncomment to make Main.java work again.
 			//fileName = dir+".java";
