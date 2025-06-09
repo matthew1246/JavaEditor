d0c06cb6ba613e564e8da54bd508e6d94abbe316
Matthew Perkovic <matthew1246@hotmail.com>
2025-06-10 00:29:14 +1000

    I maybe fucking finally did it? Got java to ask for admin priveleges.

--- withjavafx/closeandcreatejar.bat
+++ withjavafx/closeandcreatejar.bat
@@ -1,3 +1,4 @@
+cd C:\Users\Owner\Documents\javaeditor\withjavafx\
 START /B /WAIT taskkill /F /im java.exe
 START /B /WAIT taskkill /F /im javaw.exe
 del C:\Users\Owner\Documents\javaeditor\withjavafx\Main.jar
