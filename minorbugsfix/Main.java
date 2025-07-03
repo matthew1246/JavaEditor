8ccea74fcbc712ed40a636662c2290bda8187c7a
Matthew Perkovic <matthew1246@hotmail.com>
2025-07-03 20:23:26 +1000

    Fix when enter dot inside variable suggestion box the method suggestion box was sometimes not showing.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -3524,7 +3524,7 @@ class MethodSuggestionBox {
 						
 						String methodname=search_textfield.getText();
 						if(keyevent.getKeyCode() == KeyEvent.VK_PERIOD) {
-							if(!justStarted) {	
+							//if(!justStarted) {	
 								String output=currentline+".";
 								String output2=methodname;
 								if(methodname.endsWith("."))
@@ -3543,10 +3543,10 @@ class MethodSuggestionBox {
 								methods2=allobjects2;
 								labels2=getLabels(allobjects2);
 								selected_index = 0;
-							}
+							/*}
 							else {
 								justStarted = false;
-							}
+							}*/
 						}
 				
 						liveiterator = new LiveIterator<JLabel>(labels2);	
