a7e03b14e30942f7bc8ec8011dc03cb4b676eb7e
Matthew Perkovic <matthew1246@hotmail.com>
2025-07-06 20:15:08 +1000

    Beta: Fixed Main main is last parameter for method for Method Suggestion Box.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -3049,14 +3049,14 @@ class MethodSuggestionBox {
 	}
 	public String getClassName(String variablenameorclassname,String text) {
 		// Below if variable name
-		Pattern pattern4= Pattern.compile("(\\s*(\\b(public|protected|private)\\b)?\\s*(\\b(static)\\b)?\\s*([a-zA-Z<>0-9,?]+)\\s+("+variablenameorclassname+")(?=\\s*=|;|:|\\)))");	
+		Pattern pattern4= Pattern.compile("(\\s*(\\b(public|protected|private)\\b)?\\s*(\\b(static)\\b)?\\s*([a-zA-Z<>0-9,?]+)\\s+("+variablenameorclassname+")(?=\\s*=|;|:))");	
 		Matcher matcher2=pattern4.matcher(text);
-		if(matcher2.find()) {
+		if(matcher2.find()) {		
 			String classname=matcher2.group(6);
 			return classname;
 		}
 		else { 
-			Pattern pattern5=Pattern.compile("\\.*([a-zA-Z0-9]+)\\s+("+variablenameorclassname+")\\s*.+\\)");
+			Pattern pattern5=Pattern.compile("\\(.*?([a-zA-Z0-9]+)\\s*("+variablenameorclassname+")\\s*.*?\\)");
 			Matcher matcher3=pattern5.matcher(text);
 			if(!matcher3.find()) { // If static class name
 				return variablenameorclassname;
@@ -3260,6 +3260,7 @@ class MethodSuggestionBox {
 			String[] properties = editedline.split("\\.");
 			String first = properties[0];
 			String classname=getClassName(first,text);
+			JOptionPane.showMessageDialog(null,classname);
 			
 			property = getClassQuestionMark(classname,text);
 			if(property == null)
