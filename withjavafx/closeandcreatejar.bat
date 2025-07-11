0e450a207bd13e082d669a1bbf5c4d0347c934c8
Matthew Perkovic <matthew1246@hotmail.com>
2025-07-11 19:20:57 +1000

    withjavafx with latest Variable Suggestion Box code.

--- withjavafx/closeandcreatejar.bat
+++ withjavafx/closeandcreatejar.bat
@@ -1,7 +1,7 @@
 cd C:\Users\Owner\Documents\javaeditor\withjavafx\
 START /B /WAIT taskkill /F /im java.exe
 START /B /WAIT taskkill /F /im javaw.exe
-del C:\Users\Owner\Documents\javaeditor\withjavafx\HasJavaFX_ForJava22_Windows11x64.jar
+del C:\Users\Owner\Documents\javaeditor\withjavafx\Main.jar
 START /B /WAIT cmd.exe /c "C:\Program Files\Java\jdk-23\bin\jar.exe" cfm Main.jar mf.txt .
 java -jar Main.jar
 
