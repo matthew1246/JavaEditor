9eddbc704ee39b1ee177996bf2ede6288dbcc0df
Matthew Perkovic <matthew1246@hotmail.com>
2025-08-03 21:13:56 +1000

    Fixed everything: Fix crash Variable Suggestion Box when enter Stage instead of System and also fix blank document crash when using keywords.

--- withjavafx/Main.java
+++ withjavafx/Main.java
@@ -3330,8 +3330,6 @@ class AutoKeyListener {
 		List<String> variablenames = new ArrayList<String>();
 		String text = main.textarea.getText();
 		Pattern pattern2=Pattern.compile("((\\s+\\b(public|protected|private)\\b)?\\s+[a-zA-Z<>]+\\s+([a-zA-Z0-9_]+)(?=\\s*=|;))");
-		//JOptionPane.showMessageDialog(null,""+caretposition);
-		
 		Matcher matcher2=pattern2.matcher(text.substring(0,caretposition));
 		while(matcher2.find()) {
 			variablenames.add(0,matcher2.group(4));			
@@ -3401,7 +3399,7 @@ class AutoKeyListener {
 			if(caretposition > text.length()) {
 				suggestionbox.dispose();
 				return;
-			}								
+			}		
 			String first=text.substring(0,caretposition);
 			// JOptionPane.showMessageDialog(null,text.substring(caretposition,caretposition+variablename.length()));
 			
