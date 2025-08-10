983d0b757e5edf864018b0c9f3e15a728820600b
Matthew Perkovic <matthew1246@hotmail.com>
2025-08-10 21:11:34 +1000

    Fix variable suggestion box changing second line instead of intended line.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -3012,6 +3012,9 @@ class AutoKeyListener {
 	public String variablename;
 	public int caretposition;
 	public void open(String variablename,int caretposition) {
+		int selectionstart = main.textarea.getSelectionStart();
+		if(selectionstart != caretposition)
+			caretposition = selectionstart;
 		run(variablename,caretposition);
 	}
 	public void run(String variablename,int caretposition) {
@@ -3914,8 +3917,21 @@ class MethodSuggestionBox {
 							selected_index = 0;
 						}
 						if(labels2.length == 0) {
-							suggestionbox.dispose();
-							main.textarea.setCaretPosition((caretposition+1));		
+							/*suggestionbox.dispose();
+							main.textarea.setCaretPosition((caretposition+1));	
+							*/
+							/*
+							String selected = selected_label2.getText();
+							if(!ifdotbefore.equals(""))
+							*/
+							String selected = methodname;
+							if(!ifdotbefore.equals(""))
+								selected=ifdotbefore+"."+selected;
+							String firsthalf=text.substring(0,caretposition)+"."+selected;
+							//String firsthalf=text.substring(0,caretposition)+ifdotbefore+"."+selected;
+							String second =text.substring(caretposition+1,text.length());
+							main.textarea.setText(firsthalf+second);
+							main.textarea.setCaretPosition(caretposition+1+selected.length());	
 							return;
 						}
 						
