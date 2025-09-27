e6765de7f228de2354a81cac81e080851f36bb44
Matthew Perkovic <matthew1246@hotmail.com>
2025-09-27 21:46:12 +1000

    Release Candidate 2: Rename filename.

---
+++ minorbugsfix/Main.jarcloseandrenamejar.bat
@@ -0,0 +1,6 @@
+cd C:\Users\Owner\Documents\javaeditor\minorbugsfix\Main.jar
+START /B /WAIT taskkill /F /im java.exe
+START /B /WAIT taskkill /F /im javaw.exe
+START /B /WAIT cmd.exe /c RENAME Main.jar HasJavaFX_ForJava23_Windows11x64.jar
+java -jar Main.jar
+
