ad17ac0bfa231bc05abe26e97a5eaad3b2b239c4
Matthew Perkovic <matthew1246@hotmail.com>
2025-10-09 20:42:02 +1100

    .Alpha: Fixed duplicate output bug for Variable Suggestion Box.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -3704,6 +3704,7 @@ class AutoKeyListener {
 			}
 		});
 		search_textfield.addKeyListener(new KeyListener() {
+			boolean isFinished = false;
 			int count = 0;
 			String two_keys = "";
 			@Override
@@ -3782,26 +3783,28 @@ class AutoKeyListener {
 					}
 				}
 				else if(keyevent.getKeyCode() != KeyEvent.VK_ENTER && keyevent.getKeyCode() != KeyEvent.VK_DOWN && keyevent.getKeyCode() != KeyEvent.VK_UP) {
-					String input = search_textfield.getText();
-					
-					//System.out.println(count+ " "+count_release);
+					if(!isFinished) {
+						String input = search_textfield.getText();
 						
-					//if(count == count_release) {
-						if(search(input)) {
-							fillComboBox();
-						}
-						else {
-							setExtra(input);
-							EnterTextPlusExtra(input);
-							if(keyevent.getKeyChar()=='.') {
-								main.textarea.setCaretPosition(main.textarea.getCaretPosition()-1);
-								//main.curlybracekeylistener.keyPressed(keyevent);
-								KeyEvent keyevent2 = new KeyEvent(main.textarea,KeyEvent.KEY_PRESSED,System.currentTimeMillis(),0,keyevent.getKeyCode(),'.');
-								main.textarea.dispatchEvent(keyevent2);
+						//System.out.println(count+ " "+count_release);
+							
+						//if(count == count_release) {
+							if(search(input)) {
+								fillComboBox();
 							}
-						}
-					//}
-
+							else {
+								isFinished=true;
+								setExtra(input);
+								EnterTextPlusExtra(input);
+								if(keyevent.getKeyChar()=='.') {
+									main.textarea.setCaretPosition(main.textarea.getCaretPosition()-1);
+									//main.curlybracekeylistener.keyPressed(keyevent);
+									KeyEvent keyevent2 = new KeyEvent(main.textarea,KeyEvent.KEY_PRESSED,System.currentTimeMillis(),0,keyevent.getKeyCode(),'.');
+									main.textarea.dispatchEvent(keyevent2);
+								}
+							}
+						//}
+					}
 				}
 			}
 			@Override
