d2b59f673713f7a7663da20de159813ef5a1ce8b
Matthew Perkovic <matthew1246@hotmail.com>
2025-10-16 17:00:34 +1100

    withjavafx: muck. then links.innerPackages for Method Suggestion Box.

--- withjavafx/Main.java
+++ withjavafx/Main.java
@@ -5296,6 +5296,7 @@ class MethodSuggestionBox {
 						}
 					}
 				}
+				public String ifSearchTwice = "";
 				@Override
 				public void keyReleased(KeyEvent keyevent) {
 					if(keyevent.getKeyCode() == KeyEvent.VK_ENTER) {			
@@ -5306,9 +5307,8 @@ class MethodSuggestionBox {
 						String selected = selected_label2.getText();
 						CurlyBraceKeyListener.suggestionboxselected.Save(search,selected);
 						
-						/*if(!ifdotbefore.equals(""))
-							selected=ifdotbefore+"."+selected;
-						*/
+						if(!ifSearchTwice.equals(""))
+							selected=ifSearchTwice+"."+selected;
 						String firsthalf=text.substring(0,caretposition)+"."+selected;
 						//String firsthalf=text.substring(0,caretposition)+ifdotbefore+"."+selected;
 						///String second =text.substring(caretposition+1,text.length());
@@ -5332,7 +5332,11 @@ class MethodSuggestionBox {
 								String output=currentline+".";
 								String output2=methodname;
 								if(methodname.endsWith("."))
-									output2=methodname.substring(0,(methodname.length()-1));	
+ {
+									output2=methodname.substring(0,(methodname.length()-1));
+									ifSearchTwice =output2;	
+								}	
+	
 								ifdotbefore=output2;
 								
 								output=output+output2;	
