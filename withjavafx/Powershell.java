c0aab895e328b5e725bdec6aeceeb6d0221871b3
Matthew Perkovic <matthew1246@hotmail.com>
2025-10-29 01:09:35 +1100

    Before test: Make last created jar opened when create jars for all versions of java.

--- withjavafx/Powershell.java
+++ withjavafx/Powershell.java
@@ -71,8 +71,10 @@ public class Powershell {
 			}
 			output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+parentdirectory.getAbsolutePath()+"\\HasJavaFX_ForJava"+javaversionnumber+"_Windows11x64.jar mf.txt .");
 			output2.write("\n");
-			output2.write("java -jar "+parentdirectory.getAbsolutePath()+"\\HasJavaFX_ForJava"+javaversionnumber+"_Windows11x64.jar");
-			output2.write("\n");
+			if(javaversionnumber == 23) {
+				output2.write("java -jar "+parentdirectory.getAbsolutePath()+"\\HasJavaFX_ForJava"+javaversionnumber+"_Windows11x64.jar");
+				output2.write("\n");
+			}
 			// output2.close();
 		} catch (IOException ex) {
 			ex.printStackTrace();
