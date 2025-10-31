aeea9beee3497edbfd485649fb90a37a633e45a5
Matthew Perkovic <matthew1246@hotmail.com>
2025-10-31 17:45:41 +1100

    Before fix and remove quotes for gitWaitUntilFinish in new ProcessBuilder()

--- minorbugsfix/Git.java
+++ minorbugsfix/Git.java
@@ -251,8 +251,11 @@ public JButton everythingbutthekitchensink;
 					//.replace("(", "\\(")
 					//.replace(")", "\\)")
 					+"\"; ";		
-			
+			System.out.println("\""+gitbashdotexe+"\"");
+			System.out.println("\'"+echo+command+"; exec bash\'");
 			ProcessBuilder processbuilder = new ProcessBuilder("\""+gitbashdotexe+"\"","-c",	"\'"+echo+command+"; exec bash\'");
+			
+			
 			Process process=processbuilder.start();
 			process.waitFor();
 		} catch(InterruptedException ex) {
