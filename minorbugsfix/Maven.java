18ff7f7158667963d2a112add5631bfa12a99c7a
Matthew Perkovic <matthew1246@hotmail.com>
2025-12-06 23:11:55 +1100

    Alpha: Maven

--- minorbugsfix/Maven.java
+++ minorbugsfix/Maven.java
@@ -3,6 +3,10 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
 public class Maven {
 	public static void main(String[] args) {
 		Maven maven = new Maven();
@@ -16,7 +20,33 @@ public class Maven {
 		int rows=Integer.parseInt(rowsa);
 		int start_index = 0;
 		String url = "https://search.maven.org/solrsearch/select?q="+input+"&rows="+rows+"&start="+start_index+"&wt=json";
-		System.out.println(get(url));
+		String responseJson=get(url);
+		System.out.println(responseJson);
+		
+		System.out.println();
+		
+		JsonObject jsonObject = JsonParser.parseString(responseJson).getAsJsonObject();
+		JsonObject response=jsonObject.getAsJsonObject("response");
+		System.out.println(response);
+		
+		System.out.println();
+		
+		JsonArray docs=response.getAsJsonArray("docs");
+		
+		for(int i = 0; i < docs.size(); i++) {
+			JsonObject doc = docs.get(i).getAsJsonObject();
+			System.out.println(doc);
+			
+			System.out.println();
+			
+			String groupId=doc.get("g").getAsString();
+			String artifactId=doc.get("a").getAsString();
+			String version=doc.get("latestVersion").getAsString();
+			System.out.println("groupId: "+groupId+" artifactId: "+artifactId+" version: "+version);
+			
+			System.out.println();	
+		}
+		
 	}
 		
 	public OkHttpClient client = new OkHttpClient();
