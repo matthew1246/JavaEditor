1002dd357abb189229b5599928e6314ea4f79dca
Matthew Perkovic <matthew1246@hotmail.com>
2026-04-17 13:04:29 +1000

    Before test: Fix bug in previous commit.

--- minorbugsfix/Powershell.java
+++ minorbugsfix/Powershell.java
@@ -89,12 +89,10 @@ public class Powershell {
 			if(!packager.containsPackage() || !packager.isInRightFolders()) {
 				// START /B /WAIT cmd.exe /c "C:\Program Files\Java\jdk-23\bin\jar.exe" cfm Main.jar mf.txt .
 				if(javaversionnumber != 23) {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+parentdirectory.getAbsolutePath()+"\\ForJava"+javaversionnumber+"_"+main_class+".jar mf.txt .");
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+parentdirectory.getAbsolutePath()+"\\ForJava"+javaversionnumber+"_"+main_class2+".jar mf.txt .");
 				}
 				else {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+parentdirectory.getAbsolutePath()+"\\"+main_class+".jar mf.txt .");
-					output2.write("\n");
-					output2.write("java -jar "+parentdirectory.getAbsolutePath()+"\\"+main_class+".jar");
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+parentdirectory.getAbsolutePath()+"\\"+main_class2+".jar mf.txt .");
 				}
 			}
 			else { // Code is a package and package.isInRightFolder() == true
