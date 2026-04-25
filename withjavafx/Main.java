2834a0446b0a2258f22a141329c440a81465b827
Matthew Perkovic <matthew1246@hotmail.com>
2026-04-25 17:52:14 +1000

    Before withjavafx fix startup bug for Three Comboboxes.

--- withjavafx/Main.java
+++ withjavafx/Main.java
@@ -3782,6 +3782,7 @@ class SaveActionListener implements ActionListener {
 			String text = main.textarea.getText();
 			if(!main.fileName.equals("")) {
 				StoreSelectedFile storeselectedfile = new StoreSelectedFile();
+				storeselectedfile.set(main.fileName);
 				storeselectedfile.setCaretPosition(main.fileName,main.textarea.getCaretPosition());
 				boolean isSet=(new File(main.fileName)).exists();
 				PrintWriter output = new PrintWriter(main.fileName);
@@ -3826,7 +3827,7 @@ class SaveActionListener implements ActionListener {
 						}
 					}
 					
-	main.tabbedpane.setTitleAt(main.tabbedpane.getSelectedIndex(),main.fileName.replaceAll(".+\\\\",""));
+					main.tabbedpane.setTitleAt(main.tabbedpane.getSelectedIndex(),main.fileName.replaceAll(".+\\\\",""));
 					List<String> tabs=main.fileNames;
 					int tabsize = main.tabbedpane.getSelectedIndex();
 
@@ -3837,6 +3838,7 @@ class SaveActionListener implements ActionListener {
 			                       	tabs.set(tabsize,main.fileName);
 			               	}
 			               	StoreSelectedFile storeselectedfile = new StoreSelectedFile();
+			               	storeselectedfile.set(main.fileName);
 			               	storeselectedfile.setTabs(tabs);
 			               	storeselectedfile.setStarterClass(main.fileName);
 				}
