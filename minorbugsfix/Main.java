7883ba6b811263bfeda67c06b6510dca063398b4
Matthew Perkovic <matthew1246@hotmail.com>
2026-04-25 16:29:45 +1000

    Before test fix startup bug.

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -3730,6 +3730,7 @@ class SaveActionListener implements ActionListener {
 			String text = main.textarea.getText();
 			if(!main.fileName.equals("")) {
 				StoreSelectedFile storeselectedfile = new StoreSelectedFile();
+				storeselectedfile.set(main.fileName);
 				storeselectedfile.setCaretPosition(main.fileName,main.textarea.getCaretPosition());
 				boolean isSet=(new File(main.fileName)).exists();
 				PrintWriter output = new PrintWriter(main.fileName);
@@ -3774,7 +3775,7 @@ class SaveActionListener implements ActionListener {
 						}
 					}
 					
-	main.tabbedpane.setTitleAt(main.tabbedpane.getSelectedIndex(),main.fileName.replaceAll(".+\\\\",""));
+					main.tabbedpane.setTitleAt(main.tabbedpane.getSelectedIndex(),main.fileName.replaceAll(".+\\\\",""));
 					List<String> tabs=main.fileNames;
 					int tabsize = main.tabbedpane.getSelectedIndex();
 
@@ -3785,6 +3786,7 @@ class SaveActionListener implements ActionListener {
 			                       	tabs.set(tabsize,main.fileName);
 			               	}
 			               	StoreSelectedFile storeselectedfile = new StoreSelectedFile();
+			               	storeselectedfile.set(main.fileName);
 			               	storeselectedfile.setTabs(tabs);
 			               	storeselectedfile.setStarterClass(main.fileName);
 				}
