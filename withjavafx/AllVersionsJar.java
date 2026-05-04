cf7fe31147d48d6e16d783dc5b4a2606c423690c
Matthew Perkovic <matthew1246@hotmail.com>
2026-05-04 12:54:05 +1000

    Before test: Make all versions of jar without packages for withjavafx.

--- withjavafx/AllVersionsJar.java
+++ withjavafx/AllVersionsJar.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 ** This class is only if Main.jar is not running.
 */
 public class AllVersionsJar {
+	private Packager packager;
 	private String dir;
 	private Main main;
 	private String fileName;
@@ -20,7 +21,15 @@ public class AllVersionsJar {
 		this.fileName = fileName;
 		this.sal = sal;
 		this.ev4 = ev4;
-		dir = fileName.replaceAll("[^\\\\]+\\.java","");
+		packager=new Packager(main);
+		if(!packager.containsPackage() || !packager.isInRightFolders()) {
+			dir = fileName.replaceAll("[^\\\\]+\\.java","");
+		}
+		else {
+			dir=packager.classpath;
+		}
+		if(!dir.endsWith("\\"))
+			dir=dir+"\\";
 	}
 	public void Compile(boolean isJavaFX,int javaversionnumber) {
 		if(!isJavaFX) {
