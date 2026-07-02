d59a7e5055388af822d6311923b581093ee02817
Matthew Perkovic <matthew1246@hotmail.com>
2026-07-02 21:28:45 +1000

    AI fix ThreeComboboxes not loading when no backup.txt!

--- minorbugsfix/Main.java
+++ minorbugsfix/Main.java
@@ -524,6 +524,8 @@ public class Main {
 							textarea2.addMouseListener(rightclick);
 						}
 					}
+					threecomboboxes.BackgroundThreadfromScratch(fileName);
+					threecomboboxes.EDTfromScratch(fileName);
 				}	
     	 	 	};
   	 	 	swingworker.execute();
@@ -564,7 +566,9 @@ public class Main {
 				threecomboboxes.EDTfromScratch(fileName);
 			}
 		};
-		swingworker20.execute();	
+		if(fileName.equals("")) {
+			swingworker20.execute();
+		}
 		SwingWorker<Void,Void> swingworker23 = new SwingWorker<>() {
 			@Override
 			protected Void doInBackground() {
