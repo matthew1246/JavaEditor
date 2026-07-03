e1610b3df6ad199d8f40365b12d3b446602a5272
Matthew Perkovic <matthew1246@hotmail.com>
2026-07-03 18:57:50 +1000

    Fix

--- makeexeforjavafx/Main.java
+++ makeexeforjavafx/Main.java
@@ -42,7 +42,6 @@ import java.awt.event.FocusListener;
 import java.awt.event.KeyListener;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 import java.awt.FlowLayout;	
