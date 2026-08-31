0f6d41c1e2f80f88b0532b57b59164f1296e7d3f
Matthew Perkovic <matthew1246@hotmail.com>
2026-08-31 17:25:03 +1000

    Make minorbugsfix launch from subfolders.

--- minorbugsfix/start.bat
+++ minorbugsfix/start.bat
@@ -1,7 +1,7 @@
 @echo off
-if exist Main.jar (
+if exist ../../Main.jar (
 	echo java -jar Main.jar
-	java -jar Main.jar
+	java -jar ../../Main.jar
 ) else (
 	for /l %%x in (23,-1,1) do (
 		if exist ForJava%%x_Main.jar (
