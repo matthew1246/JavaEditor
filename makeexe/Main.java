04551a9a02f3b478e841824822d79c35055b0a11
Matthew Perkovic <matthew1246@hotmail.com>
2026-08-31 15:16:24 +1000

    Befor test: Makeexe work with one or more packages.

--- makeexe/Main.java
+++ makeexe/Main.java
@@ -2360,6 +2360,21 @@ Packager packager2 = new Packager(this);
 						}
 						if(!dir.endsWith("\\"))
 							dir = dir+"\\";
+						String[] options3={"One","More than One"};
+						int result = JOptionPane.showOptionDialog(
+						    null,
+						    "Do you want to make a jar with one or more packages?",
+						    "Package Selection",
+						    JOptionPane.DEFAULT_OPTION,
+						    JOptionPane.QUESTION_MESSAGE,
+						    null,
+						    options3,
+						    options3[1]  // <-- sets "More than one" as the default focused button
+						);
+						boolean isOnePackage = false;
+						if(result == 0) {
+							isOnePackage = true;
+						}
 						if(!fileName.equals("")) {
 							List<String> jars = preferences.jars;
 							if(!packager3.containsPackage() || !packager3.isInRightFolders()) { // Contains no package
@@ -2371,17 +2386,29 @@ Packager packager2 = new Packager(this);
 								}
 							}
 							else { // Package used javac.exe didn't used -d option
-								/*JOptionPane.showMessageDialog(null,"jars extract:"+dir+"jars");
-								File createdir = new File(dir+"jars");
-								if(!createdir.exists()) {
-									createdir.mkdir();
-								}*/
-								for(String jar:jars) {
-									// jar = getFileName(jar);
-									// Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir+"jars");
-									Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir);
-									process.waitFor();
-									//output.write(" "+jar);
+								if(isOnePackage) {
+									JOptionPane.showMessageDialog(null,"jars extract:"+dir+"jars");
+									File createdir = new File(dir+"jars");
+									if(!createdir.exists()) {
+										createdir.mkdir();
+									}
+									for(String jar:jars) {
+										// jar = getFileName(jar);
+										Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir+"jars");
+										// Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir);
+										process.waitFor();
+										//output.write(" "+jar);
+									}
+								}
+								else {
+									JOptionPane.showMessageDialog(null,"jars extract:"+dir);
+									for(String jar:jars) {
+										// jar = getFileName(jar);
+										// Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir+"jars");
+										Process process=commandline.run("\""+System.getProperty("java.home")+"\\bin\\jar.exe\" xf "+jar,dir);
+										process.waitFor();
+										//output.write(" "+jar);
+									}
 								}
 							}		
 						}
@@ -2452,8 +2479,14 @@ Packager packager2 = new Packager(this);
 output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .");
 								}
 								else { // packager2.isInRightFolders()
-									// output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager2.getPackageName().replace(".","\\"));
-									output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .");
+									if(isOnePackage) {
+										output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager2.getPackageName().replace(".","\\"));
+										// output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .");
+									}
+									else {
+										// output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager2.getPackageName().replace(".","\\"));
+										output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .");
+									}				
 								}
 							}
 							output2.write("\n");
@@ -2500,8 +2533,14 @@ output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\
 									input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .";
 								}
 								else { // packager2.isInRightFolders()
-									// input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager3.getPackageName().replace(".","\\");
-									input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .";
+									if(isOnePackage) {
+										input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager3.getPackageName().replace(".","\\");
+										// input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .";
+									}
+									else {
+										// input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt -C jars . "+packager3.getPackageName().replace(".","\\");
+										input="START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+main+".jar mf.txt .";
+									}				
 								}
 							}
 							JOptionPane.showMessageDialog(null,input);
