45fa2349dd0de879ef0c861a01a3d446e50507fc
Matthew Perkovic <matthew1246@hotmail.com>
2026-09-08 18:10:27 +1000

    Minor bug fix, one package fix backslash bug for creating jar.

--- minorbugsfix/PowershellOnePackage.java
+++ minorbugsfix/PowershellOnePackage.java
@@ -205,26 +205,29 @@ public class PowershellOnePackage implements Powershell {
 				String[] splited=  main_class.split("\\.");
 				main_class2 = splited[splited.length-1];
 			}
-			JOptionPane.showMessageDialog(null,"jar output directory is:"+isMoreThanOneJar.getCreateJarFolderLocation(dir));
+			String createJarFolderLocation=isMoreThanOneJar.getCreateJarFolderLocation(dir);
+			if(!createJarFolderLocation.endsWith("\\"))
+				createJarFolderLocation=createJarFolderLocation+"\\";
+			JOptionPane.showMessageDialog(null,"jar output directory is:"+createJarFolderLocation);
 			if(!packager.containsPackage() || !packager.isInRightFolders()) {
 				// START /B /WAIT cmd.exe /c "C:\Program Files\Java\jdk-23\bin\jar.exe" cfm Main.jar mf.txt .
 				if(javaversionnumber != 23 && javaversionnumber != -2) {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\ForJava"+javaversionnumber+"_"+main_class2+".jar mf.txt .");
-					main_class2=isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\ForJava"+javaversionnumber+"_"+main_class2;
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm  "+createJarFolderLocation+"ForJava"+javaversionnumber+"_"+main_class2+".jar mf.txt .");
+					main_class2=createJarFolderLocation+"ForJava"+javaversionnumber+"_"+main_class2;
 				}
 				else {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\"+main_class2+".jar mf.txt .");
-					main_class2=isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\"+main_class2;
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm  "+createJarFolderLocation+main_class2+".jar mf.txt .");
+					main_class2=createJarFolderLocation+main_class2;
 				}
 			}
 			else { // Code is a package and package.isInRightFolder() == true
 				if(javaversionnumber != 23 && javaversionnumber != -2) {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\ForJava"+javaversionnumber+"_"+main_class2+".jar mf.txt -C jars . "+packager.getPackageName().replace(".","\\"));
-					main_class2=isMoreThanOneJar.getCreateJarFolderLocation(dir)+"\\ForJava"+javaversionnumber+"_"+main_class2;
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm  "+createJarFolderLocation+"ForJava"+javaversionnumber+"_"+main_class2+".jar mf.txt -C jars . "+packager.getPackageName().replace(".","\\"));
+					main_class2=createJarFolderLocation+"ForJava"+javaversionnumber+"_"+main_class2;
 				}
 				else {
-					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm "+isMoreThanOneJar.getCreateJarFolderLocation(dir)+main_class2+".jar mf.txt -C jars . "+packager.getPackageName().replace(".","\\"));
-					main_class2=isMoreThanOneJar.getCreateJarFolderLocation(dir)+main_class2;
+					output2.write("START /B /WAIT cmd.exe /c \""+System.getProperty("java.home")+"\\bin\\jar.exe\" cfm  "+createJarFolderLocation+main_class2+".jar mf.txt -C jars . "+packager.getPackageName().replace(".","\\"));
+					main_class2=createJarFolderLocation+main_class2;
 				}
 			}						
 			output2.write("\n");
