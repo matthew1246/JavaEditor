2935e8c05a2e859905348fdf615806bce5044a96
Matthew Perkovic <matthew1246@hotmail.com>
2026-09-14 19:01:26 +1000

    Bug in previous last commit fix.

--- withjavafx/PowershellNoPackage.java
+++ withjavafx/PowershellNoPackage.java
@@ -231,11 +231,11 @@ public class PowershellNoPackage implements Powershell {
 				}
 				else { // Is Main.jar
 					if(hasJavaFX) {
-						output2.write("START /B /WAIT cmd.exe /c jar cfm "+createJarFolderLocation+"HasJavaFX_"+main_class+".jar");
+						output2.write("START /B /WAIT cmd.exe /c jar cfm "+createJarFolderLocation+"HasJavaFX_"+main_class+".jar mf.txt .");
 						fileName=createJarFolderLocation+"HasJavaFX_"+main_class+".jar";
 					}
 					else {
-						output2.write("START /B /WAIT cmd.exe /c jar cfm "+createJarFolderLocation+main_class+".jar");
+						output2.write("START /B /WAIT cmd.exe /c jar cfm "+createJarFolderLocation+main_class+".jar mf.txt .");
 						fileName=createJarFolderLocation+main_class+".jar";
 					}
 				}
