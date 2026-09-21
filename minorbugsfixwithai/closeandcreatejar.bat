939b1df47235a0ac2e954c90e003e28b757dc33d
Matthew Perkovic <matthew1246@hotmail.com>
2026-09-22 09:51:35 +1000

    minorbugsfixwithai/Main.jar has latest code!

--- minorbugsfixwithai/closeandcreatejar.bat
+++ minorbugsfixwithai/closeandcreatejar.bat
@@ -1,8 +1,8 @@
-cd C:\javaeditor\minorbugsfixwithai\
+cd C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\
 START /B /WAIT taskkill /F /im java.exe
 START /B /WAIT taskkill /F /im javaw.exe
-del C:\javaeditor\minorbugsfixwithai\Main.jar
-START /B /WAIT cmd.exe /c "C:\Program Files\MatthewJavaEditor\runtime\bin\javac.exe" *.java
-START /B /WAIT cmd.exe /c "C:\Program Files\MatthewJavaEditor\runtime\bin\jar.exe" cfm C:\javaeditor\minorbugsfixwithai\Main.jar mf.txt .
+del C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\Main.jar
+START /B /WAIT cmd.exe /c "C:\Program Files\Java\jdk-23\bin\javac.exe" -cp .;C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\gson-2.10.1.jar;C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\Muck.jar;C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\okhttp-3.0.0-RC1.jar;C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\okio-1.6.0.jar *.java
+START /B /WAIT cmd.exe /c "C:\Program Files\Java\jdk-23\bin\jar.exe" cfm C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\Main.jar mf.txt .
 
-java -jar C:\javaeditor\minorbugsfixwithai\Main.jar
+java -jar C:\Users\Owner\Documents\javaeditor\minorbugsfixwithai\Main.jar
