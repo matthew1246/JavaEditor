7d8b4809923e2c6b49917c9f7bcbd0c23f2134cb
Matthew Perkovic <matthew1246@hotmail.com>
2026-09-22 05:36:06 +1000

    makeexe fix ExtractJUnit.java bug for make C:\junit.jar

--- makeexe/ExtractJUnit.java
+++ makeexe/ExtractJUnit.java
@@ -7,8 +7,10 @@ import java.nio.file.Files;
 import java.io.IOException;
 public class ExtractJUnit {
 	public Main main;
+	protected Packager packager;
 	public ExtractJUnit(Main main) {
 		this.main = main;
+		this.packager=new Packager(main);
 		ExtractJar("junit-4.13.2.jar");
 		ExtractJar("hamcrest-core-1.3.jar");
 	}
@@ -16,20 +18,30 @@ public class ExtractJUnit {
 		try {
 			URL url=ExtractJUnit.class.getClassLoader().getResource(jar);	
 			InputStream inputstream=url.openStream();
-			String dir=main.getDirectory(main.fileName);
+			String dir = "";
+			if(!packager.containsPackage() || !packager.isInRightFolders()) {
+				dir=main.getDirectory(main.fileName);
+			}
+			else { // packager.isInRightFolders() == true
+				dir=packager.classpath;
+			}
+			if(!dir.endsWith("\\"))
+				dir=dir+"\\";
 			Path outputpath=Paths.get(dir+jar);
 			boolean needsAdmin = isDriveRoot(dir) || !canWriteToDriveRoot(dir);
 			copyJar(inputstream,outputpath,needsAdmin);
 		} catch(IOException ex) {
 			ex.printStackTrace();
 		}
-	}
+	}		
 	public boolean isDriveRoot(String dir) {
 		if(dir == null)
 			return false;
 		return dir.matches("^[A-Za-z]:\\\\+$");
 	}
 	public boolean canWriteToDriveRoot(String dir) {
+		if(dir == null)
+			return false;
 		try {
 			Path probe=Paths.get(dir+"extractjunit_probe.tmp");
 			Files.createFile(probe);
