792801503f5746cdf0f5171efd3d6203d3456faa
Matthew Perkovic <matthew1246@hotmail.com>
2026-09-24 18:22:12 +1000

    Test: package bug makes two package javaeditor.minorbugsfixwithai inside Main.java when click add package to remaining classes.

--- minorbugsfixwithai/Main.java
+++ minorbugsfixwithai/Main.java
@@ -1,3 +1,7 @@
+package javaeditor.minorbugsfixwithai;
+
+
+import javaeditor.controlf.Control_F;
 import java.nio.file.DirectoryStream;
 import java.nio.file.StandardCopyOption;
 import java.awt.KeyboardFocusManager;
